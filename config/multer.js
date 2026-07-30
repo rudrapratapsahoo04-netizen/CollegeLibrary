78e79b7286bd307ce1c441ee213c839ae6cfe7bc
Rudrapratap Sahoo <rudrapratapsahoo04@gmail.com>
2026-07-30 10:10:24 +0530

    Add upload folders

--- config/multer.js
+++ config/multer.js
@@ -1,20 +1,36 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+// Create folders automatically
+const imageDir = path.join(__dirname, "../uploads/images");
+const ebookDir = path.join(__dirname, "../uploads/ebooks");
+
+fs.mkdirSync(imageDir, { recursive: true });
+fs.mkdirSync(ebookDir, { recursive: true });
 
 const storage = multer.diskStorage({
+
     destination: (req, file, cb) => {
 
         if (file.fieldname === "image") {
-            cb(null, path.join(__dirname, "../uploads/images"));
+
+            cb(null, imageDir);
+
         } else if (file.fieldname === "ebook") {
-            cb(null, path.join(__dirname, "../uploads/ebooks"));
+
+            cb(null, ebookDir);
+
         }
 
     },
 
     filename: (req, file, cb) => {
+
         cb(null, Date.now() + path.extname(file.originalname));
+
     }
+
 });
 
 module.exports = multer({ storage });
